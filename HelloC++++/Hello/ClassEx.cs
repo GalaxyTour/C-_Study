5c0dc4844209741933aaff105201aee99e7a553f
Kim, YongJun <kyongjun1228@gmail.com>
2022-04-18 16:50:41 +0900

    2022.04.18 Study commit

--- HelloC++++/Hello/ClassEx.cs
+++ HelloC++++/Hello/ClassEx.cs
@@ -1,7 +1,100 @@
 ﻿using System;
 
+namespace ThisConstructor
+{
+    class MyClass
+    {
+        int a, b, c;
+
+        public MyClass()
+        {
+            this.a = 1234;
+            Console.WriteLine("Call MyClass()");
+        }
+
+        public MyClass(int b) :this()
+        {
+            this.b = b;
+            Console.WriteLine($"Call MyClass({b})");
+        }
+
+        public MyClass(int b, int c) :this(b)
+        {
+            this.c = c;
+            Console.WriteLine($"Call MyClass({b}, {c})");
+        }
+
+        public void PrintField()
+        {
+            Console.WriteLine($"a:{this.a}, b:{this.b}, c:{this.c}");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] afgs)
+        {
+            MyClass a = new MyClass();
+            a.PrintField();
+            Console.WriteLine();
+
+            MyClass b = new MyClass(1);
+            b.PrintField();
+            Console.WriteLine();
+
+            MyClass c = new MyClass(10, 20);
+            c.PrintField();
+        }
+    }
+}
+namespace This
+{
+    /*
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPostion()
+        {
+            return this.Position;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooch = new Employee();
+            pooch.SetName("Pooch");
+            pooch.SetPosition("Waiter");
+            Console.WriteLine($"Name: {pooch.GetName()} / Position: {pooch.GetPostion()}");
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"Name: {tigger.GetName()} / Position: {tigger.GetPostion()}");
+        }
+    }
+    */
+}
 namespace ClassCopy
 {
+    /*
     class MyClass
     {
         public int MyField1;
@@ -17,6 +110,41 @@ namespace ClassCopy
             return newCopy;
         }
     }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shallow Copy");
+
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                MyClass target = source;
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {target.MyField2}");
+            }
+
+            Console.WriteLine("Deep Copy");
+
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                MyClass target = source.DeepCopy();
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {target.MyField2}");
+            }
+        }
+    }
+    */
 }
 namespace StaticClass
 {
